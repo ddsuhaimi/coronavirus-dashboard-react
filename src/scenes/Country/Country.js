3e5fac508f35cbf8afe120539efcaa7c4e0833ff
dediacc <dedisuhaimiacc@gmail.com>
2020-04-24 01:07:16 +0700

    finish redirecting after askingcountry modal

--- src/scenes/Country/Country.js
+++ src/scenes/Country/Country.js
@@ -1,17 +1,24 @@
 import React from "react";
-import AskCountry from './components/AskCountry'
+import { BrowserRouter, Switch, Route } from "react-router-dom";
+import AskCountry from "./components/AskCountry";
+import Test from "./components/Test";
 export default function Country() {
-
   return (
-<AskCountry />    
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/country" component={AskCountry} />
+        <Route  path="/country/:countryName" component={Test} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
-
-{/* <input type="text" className= list="cars" />
+{
+  /* <input type="text" className= list="cars" />
 <datalist id="cars">
   <option>Volvo</option>
   <option>Saab</option>
   <option>Mercedes</option>
   <option>Audi</option>
-</datalist> */}
+</datalist> */
+}
