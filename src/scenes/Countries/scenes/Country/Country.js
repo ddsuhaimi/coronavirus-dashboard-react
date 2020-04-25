530d28cb4da56e318b550003d36bdf395349e746
dediacc <dedisuhaimiacc@gmail.com>
2020-04-26 02:57:14 +0700

    add loading progress in country page

--- src/scenes/Countries/scenes/Country/Country.js
+++ src/scenes/Countries/scenes/Country/Country.js
@@ -1,37 +1,58 @@
-import React, { useContext, useEffect, useState } from 'react'
-import { CoronaContext } from '../../../../context/context'
-import CaseCards from '../../../components/CaseCards'
-import TitleHeader from './components/TitleHeader'
+import React, { useContext, useEffect, useState } from "react";
+import { CoronaContext } from "../../../../context/context";
+import CaseCards from "../../../components/CaseCards";
+import TitleHeader from "./components/TitleHeader";
 export default function Country(props) {
-  const { prepareCaseCardData, all } = useContext(CoronaContext)
-  const [country, setCountry] = useState(props.match.params.country)
-  const [data, setData] = useState({})
-  const [formattedData, setFormattedData] = useState([])
+  const { prepareCaseCardData, all } = useContext(CoronaContext);
+  const [country, setCountry] = useState(props.match.params.country);
+  const [isLoading, setIsLoading] = useState(false);
+  const [data, setData] = useState({});
+  const [formattedData, setFormattedData] = useState([]);
   useEffect(() => {
     const fetchData = async () => {
+      setIsLoading(true);
       let [today, yesterday] = await Promise.all([
-        (await fetch(`https://corona.lmao.ninja/v2/countries/${country}`)).json(),
-        (await fetch(`https://corona.lmao.ninja/v2/countries/${country}?yesterday=1`)).json(),
-      ])
+        (
+          await fetch(`https://corona.lmao.ninja/v2/countries/${country}`)
+        ).json(),
+        (
+          await fetch(
+            `https://corona.lmao.ninja/v2/countries/${country}?yesterday=1`
+          )
+        ).json(),
+      ]);
       setData({
         today: today,
-        yesterday: yesterday
-      })
-    }
+        yesterday: yesterday,
+      });
+    };
 
-    fetchData()
-  }, [country])
+    fetchData();
+  }, [country]);
 
   useEffect(() => {
-    if(Object.keys(data).length !== 0) {
-      const result = prepareCaseCardData(data.today, data.yesterday)
-      setFormattedData(result)
+    if (Object.keys(data).length !== 0) {
+      const result = prepareCaseCardData(data.today, data.yesterday);
+      setFormattedData(result);
+
+      setIsLoading(false);
     }
-  }, [data])
+  }, [data]);
+
+  const Loading = (
+    <>
+      <p>Please wait while we are getting the data</p>
+      <progress class="progress is-small is-primary" max="100">
+        15%
+      </progress>{" "}
+    </>
+  );
+
   return (
-    <div className="container-fluid" style={{marginTop:'1em'}}>
-      <TitleHeader country={country}/>
+    <div className="container-fluid" style={{ marginTop: "1em" }}>
+      <TitleHeader country={country} />
+      {isLoading ? Loading : null}
       <CaseCards data={formattedData} />
     </div>
-  )
+  );
 }
